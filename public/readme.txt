2eeb4bbfc8f9dd710b00079aa3c9c2b35e747e7f
Drew Cook <drewcook29@gmail.com>
2021-09-30 21:30:00 -0600

    use yarn

--- public/readme.txt
+++
@@ -1,1 +0,0 @@
-This is a public readme file.  This acts as an asset to access from the server directly.
--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const logError = require('../middleware/logError')
-const homepage = require('../routes/homepage')
 const users = require('../routes/users')
 const auth = require('../routes/auth')
 const votes = require('../routes/votes')
@@ -8,18 +7,14 @@ const path = require('path')
 
 const setupRoutes = app => {
 	// Built-in Express middleware
-	app.use(express.json()) // parses req.body
+	// parses req.body
+	app.use(express.json())
 	// key=value&key=value, parses this and populates req.body in json
 	app.use(express.urlencoded({ extended: true }))
-	// uses a static folder for assets, and provides a route to view the assets
-	// for example http://localhost:3000/public/readme.txt
-	// app.use(express.static(path.join(__dirname, '/client/build/public')))
-
 	// Serve up our React client app
 	app.use(express.static(path.join(__dirname, '../client/build/index.html')))
 
 	// Setup API routes
-	// app.use('/', homepage)
 	app.use('/api/users', users)
 	app.use('/api/auth', auth)
 	app.use('/api/votes', votes)
